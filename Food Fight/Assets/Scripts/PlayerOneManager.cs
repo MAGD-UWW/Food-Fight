eb57c685978753986a86efbb4026de67b5073780
FrostFyres <frostfyreseo@gmail.com>
2015-11-17 11:57:15 -0600

    Food Fight Unity Project

---
+++ Food Fight/Assets/Scripts/PlayerOneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOneManager : MonoBehaviour, IGameManager {
+	public ManagerStatus status {get; private set;}
+
+	//Declare PlayerOneVariables
+
+	public void Startup() {
+		Debug.Log ("Player One Manager has started");
+		status = ManagerStatus.Started;
+	}
+}
---
+++ Food Fight/Assets/Scripts/PlayerTwoManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwoManager : MonoBehaviour, IGameManager {
+	public ManagerStatus status { get; private set; }
+
+	//Declare Player Two variables
+
+	public void Startup() {
+		Debug.Log ("Player Two Manager has started");
+		status = ManagerStatus.Started;
+	}
+}
