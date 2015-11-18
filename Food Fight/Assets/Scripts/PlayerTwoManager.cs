a0b2701ef5237c8609b2070bec322a0f6ec84f49
FrostFyres <frostfyreseo@gmail.com>
2015-11-18 15:46:28 -0600

    Edited Variables

--- Food Fight/Assets/Scripts/PlayerTwoManager.cs
+++ Food Fight/Assets/Scripts/PlayerTwoManager.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class PlayerTwoManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status { get; private set; }
-	public int playerTwoHealth { get; private set; }
-	public int playerTwoMaxHalth { get; private set; }
-	public int playerTwoFoodCount { get; private set; }
-	public int playerTwoMaxFoodCount { get; private set; }
-	public int playerTwoActionPoints { get; private set; }
+	public int playerTwoHealth { get; set; }
+	public int playerTwoMaxHalth { get; set; }
+	public int playerTwoFoodCount { get; set; }
+	public int playerTwoMaxFoodCount { get; set; }
+	public int playerTwoActionPoints { get; set; }
 
 
 	public void Startup() {
