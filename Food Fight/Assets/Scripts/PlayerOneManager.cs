4a2d7cbde40f33dc2316f1c21a8bcf74f8c3f398
FrostFyres <frostfyreseo@gmail.com>
2015-11-18 15:08:57 -0600

    Revert "Variables Changes"
    This reverts commit e66e932dccb0d0fee451e98ed4df15094d437e29.

--- Food Fight/Assets/Scripts/PlayerOneManager.cs
+++ Food Fight/Assets/Scripts/PlayerOneManager.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class PlayerOneManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status {get; private set;}
-	public int playerOneHealth { get; set; }
-	public int playerOneMaxHalth { get; set; }
-	public int playerOneFoodCount { get; set; }
-	public int playerOneMaxFoodCount { get; set; }
-	public int playerOneActionPoints { get; set; }
-	//Add any variables you can think of let me know
+	public int playerOneHealth { get; private set; }
+	public int playerOneMaxHalth { get; private set; }
+	public int playerOneFoodCount { get; private set; }
+	public int playerOneMaxFoodCount { get; private set; }
+	public int playerOneActionPoints { get; private set; }
 
 	public void Startup() {
 		Debug.Log ("Player One Manager has started");
--- Food Fight/Assets/Scripts/PlayerTwoManager.cs
+++ Food Fight/Assets/Scripts/PlayerTwoManager.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class PlayerTwoManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status { get; private set; }
-	public int playerTwoHealth { get; set; }
-	public int playerTwoMaxHalth { get; set; }
-	public int playerTwoFoodCount { get; set; }
-	public int playerTwoMaxFoodCount { get; set; }
-	public int playerTwoActionPoints { get; set; }
-	//Add any variables you can think of let me know
+	public int playerTwoHealth { get; private set; }
+	public int playerTwoMaxHalth { get; private set; }
+	public int playerTwoFoodCount { get; private set; }
+	public int playerTwoMaxFoodCount { get; private set; }
+	public int playerTwoActionPoints { get; private set; }
 
 
 	public void Startup() {
