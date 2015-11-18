c31ac2bf2ae3148acd7a39686a7513c90aed86a8
rioter00 <nickthwang@gmail.com>
2015-11-18 14:16:53 -0600

    Nick's change to ChoiceManager
    added lines 7-9

--- Food Fight/Assets/Scripts/ChoiceManager.cs
+++ Food Fight/Assets/Scripts/ChoiceManager.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class ChoiceManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status {get; private set;}
 
+	// this is Nick's awesome edit!
+
+
 	//Declare Choice variables
 
 	public void Startup() {
