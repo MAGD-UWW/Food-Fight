dc28fa650e363698f4a5903b727d1521e590d01d
dodgeaw28 <dodgeaw28@uww.edu>
2015-11-18 15:25:46 -0600

    grab is there not in eric variables yet
    still have to change to Eric's variables but grab does work
    also fixed harth to health

--- Food Fight/Assets/Scripts/PlayerOneManager.cs
+++ Food Fight/Assets/Scripts/PlayerOneManager.cs
@@ -4,14 +4,43 @@ using System.Collections;
 public class PlayerOneManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status {get; private set;}
 	public int playerOneHealth { get; set; }
-	public int playerOneMaxHalth { get; set; }
+	public int playerOneMaxHealth { get; set; }
 	public int playerOneFoodCount { get; set; }
 	public int playerOneMaxFoodCount { get; set; }
 	public int playerOneActionPoints { get; set; }
 	//Add any variables you can think of let me know
 
+	int carrying;
+	GameObject food;
+
 	public void Startup() {
 		Debug.Log ("Player One Manager has started");
 		status = ManagerStatus.Started;
+	}
+
+	void Start () {
+		food = GameObject.FindWithTag ("food");
+		
+	}
+
+	void Update () {
+		
+		if (carrying == 3) {
+			carry (food);
+		} else {
+			pickup ();
+		}
+	}
+
+	void carry(GameObject o){
+			
+	}
+
+	void pickup(){
+		if (Input.GetKeyDown ("l")) {
+			carrying = carrying + 1;
+			Debug.Log ("Player 1:");
+			Debug.Log (carrying);
+		}
 	}
 }
--- Food Fight/Assets/Scripts/PlayerTwoManager.cs
+++ Food Fight/Assets/Scripts/PlayerTwoManager.cs
@@ -4,15 +4,43 @@ using System.Collections;
 public class PlayerTwoManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status { get; private set; }
 	public int playerTwoHealth { get; set; }
-	public int playerTwoMaxHalth { get; set; }
+	public int playerTwoMaxHealth { get; set; }
 	public int playerTwoFoodCount { get; set; }
 	public int playerTwoMaxFoodCount { get; set; }
 	public int playerTwoActionPoints { get; set; }
 	//Add any variables you can think of let me know
 
+	int carrying;
+	GameObject food;
 
 	public void Startup() {
 		Debug.Log ("Player Two Manager has started");
 		status = ManagerStatus.Started;
+	}
+
+	void Start () {
+		food = GameObject.FindWithTag ("food");
+		
+	}
+
+	void Update () {
+		
+		if (carrying == 3) {
+			carry (food);
+		} else {
+			pickup ();
+		}
+	}
+	
+	void carry(GameObject o){
+		
+	}
+	
+	void pickup(){
+		if (Input.GetKeyDown ("d")) {
+			carrying = carrying + 1;
+			Debug.Log ("Player 2:");
+			Debug.Log (carrying);
+		}
 	}
 }
