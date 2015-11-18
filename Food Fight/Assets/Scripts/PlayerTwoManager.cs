7c8995165b813c9829606549b834a459eaa8fbda
dodgeaw28 <dodgeaw28@uww.edu>
2015-11-18 15:37:44 -0600

    Eric's variables with Grab Function
    Should contain the variables you were looking for

--- Food Fight/Assets/Scripts/PlayerTwoManager.cs
+++ Food Fight/Assets/Scripts/PlayerTwoManager.cs
@@ -4,43 +4,43 @@ using System.Collections;
 public class PlayerTwoManager : MonoBehaviour, IGameManager {
 	public ManagerStatus status { get; private set; }
 	public int playerTwoHealth { get; set; }
+	//Anson fixed spelling
 	public int playerTwoMaxHealth { get; set; }
 	public int playerTwoFoodCount { get; set; }
 	public int playerTwoMaxFoodCount { get; set; }
 	public int playerTwoActionPoints { get; set; }
 	//Add any variables you can think of let me know
 
-	int carrying;
+	//Anson what the player is picking up
 	GameObject food;
 
 	public void Startup() {
 		Debug.Log ("Player Two Manager has started");
 		status = ManagerStatus.Started;
-	}
-
-	void Start () {
+		//Anson need to reference the food 
 		food = GameObject.FindWithTag ("food");
-		
 	}
 
-	void Update () {
-		
-		if (carrying == 3) {
+	void Update () {	
+		//Anson When the food count reaches 3 they can pickup no more
+		if (playerTwoFoodCount == 3) {
 			carry (food);
 		} else {
 			pickup ();
 		}
 	}
-	
+
+	//Anson this is Player 2 max I think....
 	void carry(GameObject o){
 		
 	}
-	
+
+	//Anson this is player 2 pressing L to add food
 	void pickup(){
-		if (Input.GetKeyDown ("d")) {
-			carrying = carrying + 1;
+		if (Input.GetKeyDown ("l")) {
+			playerTwoFoodCount = playerTwoFoodCount + 1;
 			Debug.Log ("Player 2:");
-			Debug.Log (carrying);
+			Debug.Log (playerTwoFoodCount);
 		}
 	}
 }
