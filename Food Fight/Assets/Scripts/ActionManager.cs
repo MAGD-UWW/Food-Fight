6c925c2c87af00f81f98da651231a722bdec4521
FrostFyres <frostfyreseo@gmail.com>
2015-11-29 16:41:33 -0600

    Added comments and changed code in the managers

--- Food Fight/Assets/Scripts/ActionManager.cs
+++ Food Fight/Assets/Scripts/ActionManager.cs
@@ -9,9 +9,29 @@ public class ActionManager : MonoBehaviour, IGameManager {
 	public PlayerTwoManager player2;
 	public Vector2[] playerOneChoices;
 	public Vector2[] playerTwoChoices;
-	public enum playerActions{ move, toss, grab, skip };
-	public enum playerOneGrid { buttonTab, buttonQ, buttonW, buttonE, buttonR };
-	public enum playerTwoGrid { buttonI, buttonO, buttonP, buttonLS, button5RS }; //LS = left square [ || RS = right square ]
+	//Organized the Enumerators 11/26/15 Eric Olson
+	public enum playerActions{ //Actions the players move
+		move, 
+		toss, 
+		grab, 
+		skip 
+	};
+	//Should I instead, make these "move" actions within the playerActions enumerator? - 11/26/15 Eric Olson
+	public enum playerOneGrid { //Buttons player one uses 
+		buttonTab, 
+		buttonQ, 
+		buttonW, 
+		buttonE, 
+		buttonR 
+	};
+	//Should I instead, make these "move" actions within the playerActions enumerator? - 11/26/15 Eric Olson
+	public enum playerTwoGrid { //Buttons player two uses
+		buttonI, 
+		buttonO, 
+		buttonP, 
+		buttonLS, 
+		buttonRS 
+	}; //LS = left square bracket || RS = right square bracket
 
 	public void Startup () {
 		//Enumerators
@@ -20,10 +40,13 @@ public class ActionManager : MonoBehaviour, IGameManager {
 		//playerOneActions = playerActions.move;
 		player1 = GameObject.Find ("Manager").GetComponent<PlayerOneManager>();
 		player2 = GameObject.Find ("Manager").GetComponent<PlayerTwoManager>();
+		//Figure out how to use Enumerators as Index's for Arrays -Eric Olson 11/26/15
 		playerOneChoices = new Vector2[player1.playerOneActionPoints];
-		playerTwoChoices = new Vector2[player2.playerTwoTotalActionPoints];
-		//X = move, throw, grab(3 buttons) || y = Grid movement(5 buttons)
+		playerTwoChoices = new Vector2[player2.playerTwoActionPoints]; //Changed from Total Action Points to ActionPoints 11/26/15 Eric Olson
 		/**
+			PLAYER GRID OR PLAYER ACTIONS?  SHOULD GRID GO IN P1Manager / P2Manager? Reference P1Movement and P2Movement? - Eric Olson 11/26/15
+			**Many ways of going about this
+			**Grid selection THEN action, (x,y) MOVE or (x,y) THROW or (x,y) GRAB etc.
 		playerOneChoices[0]( playerActions, playerGrid); //Player1 first choice
 		playerOneChoices[1]( playerActions, playerGrid); //Player1 second choice
 		playerOneChoices[2]( playerActions, playerGrid); //Player1 third choice
