ec9cd0f52b778fad9cd193de8097980a02a38c33
christianheide <heidecr23@uww.edu>
2015-11-30 15:56:21 -0600

    created the player's action script
    it's called actionScript

---
+++ Food Fight/Assets/Scripts/actionScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actionScript : MonoBehaviour {
+	public PlayerOneManager player1;
+	public PlayerTwoManager player2;
+	public ActionManager playerActions{ move, toss, grab, skip };
+
+	// Use this for initialization
+	void Start () {
+		player1 = GameObject.Find("Manager").GetComponent<PlayerOneManager>();
+		player2 = GameObject.Find("Manager").GetComponent<PlayerTwoManager>();
+		playerActions = GameObject.Find ("Manager").GetComponent<ActionManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Actions(){
+		playerActions.playerOneChoices[player1.playerOneActionPoints];
+	}
+}
