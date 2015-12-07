9537b6a4cb39b27e86b17183acc9c6b8a7980c47
FrostFyres <frostfyreseo@gmail.com>
2015-12-07 10:44:40 -0600

    Added SecretScript

---
+++ Food Fight/Assets/Scripts/SecretScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretScript : MonoBehaviour {
+	public PlayerOneManager player1;
+	public PlayerTwoManager player2;
+
+	void Start() {
+		player1 = GameObject.Find ("Manager").GetComponent<PlayerOneManager>();
+		player2 = GameObject.Find ("Manager").GetComponent<PlayerTwoManager>();
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.PageUp)) {
+			player1.playerOneHealth = 0;
+		}
+		if(Input.GetKeyDown (KeyCode.PageDown)) {
+			player2.playerTwoHealth = 0;
+		}
+	}
+}
